928fdaa15e457beeb10a68569f776c59a51f5cb3
zhangyf511 <498104227@qq.com>
2022-06-22 11:51:40 +0800

    popup

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import {
   Canvas,
   Polygon,
   popup,
+  GeoJSON,
 } from 'leaflet';
 
 //创建高德地图
@@ -33,7 +34,7 @@ var amap = new AMap.Map('amap', {
 const map = new Map('map', {
   renderer: new Canvas(),
 });
-map.setView([39.909186, 116.397411], 14);
+map.setView([39.909186, 116.397411], 8);
 
 // Map和高德地图耦合
 map.on('zoom', (evt) => {
@@ -44,7 +45,7 @@ map.on('move', (evt) => {
   amap.setZoomAndCenter(evt.target.getZoom(), [pt.lng, pt.lat]);
 });
 
-// 点
+// 1个点
 new Marker([39.909176, 116.397411], {
   icon: new Icon({
     iconUrl:
@@ -54,16 +55,57 @@ new Marker([39.909176, 116.397411], {
   }),
 }).addTo(map);
 
-const layerGroup = new GeoJSON(geoData, {
-    pointToLayer: (geoJsonPoint, latlng) => {
-      return new Marker(latlng, {
-        icon: new Icon({
-          iconUrl:
-            'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-          iconSize: [25, 41],
-          iconAnchor: [12, 41],
-        }),
-      });
-    },
-  });
-  layerGroup.addTo(map);
+//====多个点start========
+const geoData = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: {
+        NAME: '北三环一号',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [116.3911235332489, 39.95286642741858],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        NAME: '北三环二号',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [116.38795852661131, 39.94758202338572],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        NAME: '北三环三号',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [116.39014720916748, 39.95090487138684],
+      },
+    },
+  ],
+};
+
+const layerGroup = new GeoJSON(geoData, {
+  pointToLayer: (geoJsonPoint, latlng) => {
+    return new Marker(latlng, {
+      icon: new Icon({
+        iconUrl:
+          'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+      }),
+    });
+  },
+});
+// layerGroup.addTo(map);
+//====多个点end========
+layerGroup.addTo(map).bindPopup((layer) => {
+  return layer.feature.properties('NAME');
+});
